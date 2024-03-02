104f5b62850becb7c9b51ddcb49fa3d13a4fd8f4
Andrey <127634916+iycscars@users.noreply.github.com>
2024-03-02 12:08:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,229 @@
+
+
+const meusAlbuns = [
+  {
+    "nome_do_album": "<a href='https://open.spotify.com/intl-pt/album/7Lne0pTstB5jEBVkoptspK' target='_blank'>Francisco Oceano</a>",
+    "nome_da_banda": "Rodrigo Zin",
+    "ano_do_album": 2018,
+    "imagem_da_capa_do_album": "https://images.genius.com/8c805cb1c9b8bd5a1acf79c1beb50819.1000x1000x1.jpg"
+  },
+  {
+    "nome_do_album": "<a href='https://open.spotify.com/intl-pt/album/5ofjCcIWQHYvgAU1uaxwyi' target='_blank'>Invasor </a>",
+    "nome_da_banda": "Rodrigo Zin",
+    "ano_do_album": 2023,
+    "imagem_da_capa_do_album": "https://i.scdn.co/image/ab67616d0000b2730796832ffa9022f359a37412"
+  },
+  {
+    "nome_do_album": "<a href='https://open.spotify.com/intl-pt/album/0GvJEsPWf7aRrgkSVXzXRj' target='_blank'>Grana Azul</a>",
+    "nome_da_banda": "Rodrigo Zin",
+    "ano_do_album": 2019,
+    "imagem_da_capa_do_album": "https://i.scdn.co/image/ab67616d0000b273825efcb1ce20bcd1a30ffabc"
+  },
+  {
+    "nome_do_album": "<a href='https://open.spotify.com/intl-pt/track/2W3ZpQg9i6lE6kmHbcdu9N' target='_blank'>Cracker Island</a>",
+    "nome_da_banda": "Gorillaz",
+    "ano_do_album": 2023,
+    "imagem_da_capa_do_album": "https://musicainstantanea.com.br/wp-content/uploads/2022/08/Gorillaz-Cracker-Island.jpeg"
+  },
+  {
+    "nome_do_album": "<a href='https://open.spotify.com/intl-pt/album/5I9K0u8r52jp4Y2bF8mOaz' target='_blank'>Song Machine, Season One: Strange Timez</a>",
+    "nome_da_banda": "Gorillaz",
+    "ano_do_album": 2020,
+    "imagem_da_capa_do_album": "https://upload.wikimedia.org/wikipedia/en/5/5d/Song_Machine_Season_One_Strange_Timez.png"
+  },
+  {
+    "nome_do_album": "<a href='https://open.spotify.com/intl-pt/track/6BL4PpswVjH9BAajbIbpmZ' target='_blank'>Tomorrow Comes Today</a>",
+    "nome_da_banda": "Gorillaz",
+    "ano_do_album": 2000,
+    "imagem_da_capa_do_album": "https://static.wikia.nocookie.net/kong/images/3/33/TomorrowComesTodayEP.jpg/revision/latest?cb=20230701013938"
+  },
+  {
+    "nome_do_album": "<a href='https://open.spotify.com/intl-pt/album/4ML88AyBHneIkmTHJunWCC' target='_blank'>Pursuit</a>",
+    "nome_da_banda": "Stuck in the Sound",
+    "ano_do_album": 2012,
+    "imagem_da_capa_do_album": "https://i.scdn.co/image/ab67616d0000b273d563ba2c03dea998cbabadb8"
+  },
+  {
+    "nome_do_album": "<a href='https://open.spotify.com/intl-pt/album/24e49t2eGI5UV88f1uLTOe' target='_blank'>Survivor</a>",
+    "nome_da_banda": "Stuck in the Sound",
+    "ano_do_album": 2016,
+    "imagem_da_capa_do_album": "https://i.scdn.co/image/ab67616d0000b273d1c59032d8d7e811b259ba9b"
+  },
+  {
+    "nome_do_album": "<a href='https://open.spotify.com/intl-pt/album/1485FZSX7HEo5vwK1KHk8c' target='_blank'>16 Dreams a Minute</a>",
+    "nome_da_banda": "Stuck in the Sound",
+    "ano_do_album": 2024,
+    "imagem_da_capa_do_album": "https://cdns-images.dzcdn.net/images/cover/83be6034bfb2de2ac9822b13cb4b9d78/500x500.jpg"
+  },
+  {
+    "nome_do_album": "<a href='https://open.spotify.com/intl-pt/album/4aGp0loKrV89iJdJoksnYN' target='_blank'>Little Armageddon</a>",
+    "nome_da_banda": "Skip the Use",
+    "ano_do_album": 2013,
+    "imagem_da_capa_do_album": "https://i.scdn.co/image/ab67616d0000b273868a72a6bbe0238163ba7c99"
+  },
+  {
+    "nome_do_album": "<a href='https://open.spotify.com/intl-pt/album/7pqfT0igQtjHaDnYChZjCl' target='_blank'>Human Disorder</a>",
+    "nome_da_banda": "Skip the Use",
+    "ano_do_album": 2022,
+    "imagem_da_capa_do_album": "https://i.scdn.co/image/ab67616d0000b27319d4de81d458c9840c88e56b"
+  },
+  {
+    "nome_do_album": "<a href='https://open.spotify.com/intl-pt/album/2D9zCv79mWQw6j3nRTcf4N' target='_blank'>Can Be Late</a>",
+    "nome_da_banda": "Skip the Use",
+    "ano_do_album": 2012,
+    "imagem_da_capa_do_album": "https://i.scdn.co/image/ab67616d00001e02dad5c8b19944661879bd77ac"
+  },
+  {
+    "nome_do_album": "<a href='https://open.spotify.com/intl-pt/album/6cunQQ7YZisYOoiFu2ywIq' target='_blank'>Up All Night</a>",
+    "nome_da_banda": "One Direction",
+    "ano_do_album": 2011,
+    "imagem_da_capa_do_album": "https://upload.wikimedia.org/wikipedia/en/9/96/One_direction_up_all_night_albumcover.jpg"
+  },
+  {
+    "nome_do_album": "<a href='https://open.spotify.com/intl-pt/album/2sWX3HYnZjPZ9MrH6MFsBt' target='_blank'>Take Me Home</a>",
+    "nome_da_banda": "One Direction",
+    "ano_do_album": 2012,
+    "imagem_da_capa_do_album": "https://images.musicstore.de/images/0960/hal-leonard-one-direction-take-me-home_1_NOT0009872-000.jpg"
+  },
+  {
+    "nome_do_album": "<a href='https://open.spotify.com/intl-pt/album/7p1fX8aUySrBdx4WSYspOu' target='_blank'>Midnight Memories</a>",
+    "nome_da_banda": "One Direction",
+    "ano_do_album": 2013,
+    "imagem_da_capa_do_album": "https://m.media-amazon.com/images/I/91KvLzMW0RL._AC_UF1000,1000_QL80_.jpg"
+  },
+]
+
+function organizaEmLinhasEColunas(albuns) {
+  const numeroDeColunas = 13414
+  const numeroDeLinhas = Math.ceil(albuns.length / numeroDeColunas)
+
+  let linhas = new Array(numeroDeLinhas)
+
+  let indiceAlbuns = 0;
+
+  for (let i = 0; i < numeroDeLinhas; i++) {
+    linhas[i] = new Array(numeroDeColunas)
+
+    for (let j = 0; j < numeroDeColunas; j++) {
+      if (indiceAlbuns < albuns.length) {
+        linhas[i][j] = albuns[indiceAlbuns++]
+      }
+    }
+  }
+
+  return linhas
+}
+
+function criaCardHtmlParaAlbum(album) {
+  return `
+  <div class="card col-4 search-item">
+    <img class="card-img-top"
+      src="${album.imagem_da_capa_do_album}"
+      alt="">
+    <div class="card-body">
+      <h5 class="card-title">${album.nome_do_album}</h5>
+      <p class="card-text">${album.nome_da_banda} - <b>${album.ano_do_album}</b></p>
+    </div>
+  </div>
+  `
+}
+
+function criaLinhaDeAlbuns(uma_linha) {
+  const div = document.createElement("div")
+  div.classList.add("row")
+  div.innerHTML = uma_linha.map(coluna => criaCardHtmlParaAlbum(coluna)).join("\n")
+  return div
+}
+
+function criaListaDeAlbuns(linhas) {
+  const div = document.createElement("div")
+  div.classList.add("col-lg-12", "px-0", "container")
+  div.setAttribute("id", "album-list")
+
+  linhas.forEach(linha => {
+    div.appendChild(criaLinhaDeAlbuns(linha))
+  });
+
+  return div
+}
+
+function atualizaListaDeAlbuns() {
+  const listaDeAlbuns = document.getElementById("album-list")
+
+  listaDeAlbuns.replaceWith(criaListaDeAlbuns(organizaEmLinhasEColunas(meusAlbuns)))
+}
+
+function randomizaListaDeAlbuns() {
+  const listaDeAlbuns = document.getElementById("album-list");
+  
+  listaDeAlbuns.replaceWith(criaListaDeAlbuns(organizaEmLinhasEColunas(meusAlbuns.sort ( ( ) => Math . random ( ) - 0.5 ))))
+}
+
+// getElementById("searchInput").addEventListener("keydown", (event) => {});
+// onkeydown = (event) => {
+//   console.log("Ramo")
+// };
+
+
+
+const searchInput = document.getElementById("searchInput");
+const albumNames = document.getElementsByClassName("card-title");
+
+searchInput.addEventListener("keyup", (event) => {
+  const { value } = event.target;
+  const searchQuery = value.toLowerCase();
+  
+  for (const nameElement of albumNames) {
+
+      let name = nameElement.textContent.toLowerCase();
+
+      if (name.includes(searchQuery)) {
+          nameElement.parentElement.parentElement.style.display = "block";
+
+      } else {
+          nameElement.parentElement.parentElement.style.display = "none";
+
+      }
+  }
+});
+
+function form12() {
+  const albumDiv = document.getElementsByClassName("form")
+  if (albumDiv[0].style.display != 'none') {
+    albumDiv[0].style.setProperty('Display', 'none');
+    albumDiv[1].style.setProperty('Display', 'none');
+    document.getElementById("albumForm").style.setProperty('Display', 'block')
+    
+  } else {
+    albumDiv[0].style.setProperty('Display', 'block');
+    albumDiv[1].style.setProperty('Display', 'block');
+    document.getElementById("albumForm").style.setProperty('Display', 'none')
+  }
+}
+
+document.getElementById("submitButton2").addEventListener("click", function() {
+  if (document.getElementById("albumName").value === "") {
+    alert ("Insira o nome do Album!");
+  } else if (document.getElementById("bandName").value === "") {
+    alert ("Insira o nome da Banda!");
+  } else if (document.getElementById("albumYear").value === "") {
+    alert ("Insira o ano em que o Album foi lançado!");
+  } else if (document.getElementById("albumLink").value === "") {
+    alert ("Insira o link do album!");
+  } else if (document.getElementById("albumCover").value === "") {
+    alert ("Insira o link da capa do Album!");
+  } else {
+  let albumName = document.getElementById("albumName").value;
+  let bandName = document.getElementById("bandName").value;
+  let albumYear = document.getElementById("albumYear").value;
+  let albumLink = document.getElementById("albumLink").value;
+  let albumCover = document.getElementById("albumCover").value;
+  meusAlbuns.push({
+            "nome_do_album": '<a href="' + albumLink + '">' + albumName + '</a>',
+            "nome_da_banda": bandName,
+            "ano_do_album": albumYear,
+            "imagem_da_capa_do_album": albumCover
+})
+form12()
+atualizaListaDeAlbuns() }
+});
